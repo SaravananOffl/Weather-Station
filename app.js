ada2e4c6ecf046dd7cbdd682292e5a54827b7169
Saravananoffl <sgoffl1603@gmail.com>
2018-05-12 19:40:44 +0530

    Add routes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
-
+const request = require('request');
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine','ejs');
+
+
+const apiKey ='e931741e1fe41345b9a1b21ed77cac63';
 
 app.get('/',(req,res)=>{
 
@@ -20,7 +23,24 @@ app.get('/search',(req,res)=>{
 app.post('/search',(req,res)=>{
 
     console.log(req.body);
-    res.render('search_results',{cityname: req.body.ctyname});
+    let city = req.body.cityname;
+    url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    request(url,(err,response,body)=>{
+        if(!err) {
+        const weather = JSON.parse(body);
+            console.log("NO ");
+            res.render('search_results',{cityname: req.body.ctyname, weather: weather});
+
+        }
+        else{
+            res.send("Error");
+        }
+
+    });
+
+
+
+
 
 });
 
