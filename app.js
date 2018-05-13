c18ae794ec88df8c705840bd80a95600eff85e6e
Saravananoffl <sgoffl1603@gmail.com>
2018-05-13 12:14:37 +0530

    Add exceptions

--- app.js
+++ app.js
@@ -20,16 +20,12 @@ app.get('/search',(req,res)=>{
     res.render('index');
 });
 
-app.get('*',(req,res)=>{
-
-res.render('default_route');
-});
 app.post('/search',(req,res)=>{
 
     console.log(req.body);
     let city = req.body.cityname;
     url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
+    if(!(city ==='')){
     request(url,(err,response,body)=>{
         const weather = JSON.parse(body);
         if(err || weather.cod ==='404') {
@@ -41,14 +37,19 @@ app.post('/search',(req,res)=>{
 
         }
 
-    });
-
-
-
-
+    }
+    );}
+    else{
+        res.send('<div> <h1> No City Name Entered</h1> <a href="/">Go back</a></div> ')
+    }
 
 });
 
+
+app.get('*',(req,res)=>{
+
+    res.render('default_route');
+});
 
 
 
